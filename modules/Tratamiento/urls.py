aa0565630a80e284951ce981367c4d530b3846b9
iiventura <ireneven@ucm.es>
2019-02-27 21:36:47 +0100

    Nuevo  Tratamiento

--- modules/Tratamiento/urls.py
+++ modules/Tratamiento/urls.py
@@ -2,10 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path('nuevo', views.nuevo, name='app_nuevo'),
+    #path('lista', views.listar, name='app_listar'),
+    #path('<int:pk>/eliminar', views.eliminar, name='app_eliminar'),
+    # path('<int:pk>/modificar', views.modificar, name='app_modificar'),
 ]
 """
-  path('nuevoTratamiento', views.nuevo, name='app_nuevoTratamiento'),
+  
    path('modificarTratamiento', views.modificar, name='app_modificarTratamiento'),
    path('borrarTratamiento', views.borrar, name='app_borrarTratamiento'),
    path('listarTratamientos', views.listar, name='app_listarTratamientos'),
--- modules/Tratamiento/views.py
+++ modules/Tratamiento/views.py
@@ -1,3 +1,33 @@
+from django.contrib import messages
 from django.shortcuts import render
 
-# Create your views here.
+from modules.Maquina.models import Maquina
+from .models import Tratamiento
+from .forms import FormTratamientoInsert
+
+
+def nuevo(request):
+   if request.method == "POST":
+      form = FormTratamientoInsert(request.POST)
+
+      if form.is_valid():
+         datos = form.cleaned_data
+
+         # recogemos los datos
+         nomTra = datos.get("nombre")
+         fecha = datos.get("descripcion")
+         tipo = datos.get("maquina")
+
+         if not Tratamiento.objects.filter(nombre=nomTra):  # todavia se puede guardar una maquina mas
+            instMaquina = Maquina.objects.get(nombre=tipo)
+            t = Tratamiento(nombre=nomTra, descripcion=fecha, maquina=instMaquina);
+            t.save()
+            return render(request, 'tnuevo.html', {'form': form})
+
+         else:
+            messages.error(request, 'El tratamiento ya existe.')
+            messages.error(request, '')
+   else:
+      form = FormTratamientoInsert()
+
+   return render(request, 'tnuevo.html', {'form': form})
