37325fff20c80007eed0c40563673aff336c9e16
iiventura <ireneven@ucm.es>
2019-02-27 13:35:16 +0100

    Nuevo Producto

--- modules/Sala/urls.py
+++ modules/Sala/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
-
+    path('nuevo', views.nuevo, name='nuevo_producto'),
+    #path('lista', views.listar, name='app_listar'),
 ]
 
 """
--- modules/Sala/views.py
+++ modules/Sala/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from .forms import FormSalaInsert
+from .models import Sala
+from django.contrib import messages
 
-# Create your views here.
+
+def nuevo(request):
+    if request.method == "POST":
+        form = FormSalaInsert(request.POST)
+
+        if form.is_valid():
+            datos = form.cleaned_data
+
+            # recogemos los datos
+            nomSala = datos.get("nombre")
+
+            if not Sala.objects.filter(nombre=nomSala):  # todavia se puede guardar una maquina mas
+
+                s = Sala(nombre=nomSala)
+                s.save()
+
+                return render(request, 'nuevoP.html', {'form': form})
+            else:
+                messages.error(request, 'La sala ya existe.')
+                messages.error(request, '')
+    else:
+        form = FormSalaInsert()
+
+    return render(request, 'nuevoP.html', {'form': form})
