e8a1f31e2a2cd28092304b6a2f0adac40ee94654
Irene Ventura <ireneven@ucm.es>
2019-03-07 20:16:38 +0100

    Revert "Revert "fechas publicidad""
    This reverts commit 83c199c4438c236eec36d808cdcec42df738c321.

--- modules/Publicidad/forms.py
+++ modules/Publicidad/forms.py
@@ -26,18 +26,26 @@ def clienteChoice():
 
     return tuple(resultado)
 
+def formatoDate():
+
+    fecha = timezone.now().date()
+    fecha = str(fecha)
+    #en la bbdd se guarda yyyy-mm-dd cambiamso para que sea dd-mm-yyy
+    cad = fecha.split('-')
+    cad = cad[2] + '-' + cad[1] + '-' + cad[0]
+
+    return cad
 
 class FormPublicidadInsert(forms.Form):
 
-    fechainicio = forms.DateField(label=("Inicio"),
-        initial=timezone.now().date(),
-        input_formats=['%Y-%m-%d'],
-        widget=forms.DateInput(format='%Y-%m-%d'))
+    fecha = formatoDate()
 
-    fechafin = forms.DateField(label=("Fin"),
-        initial=timezone.now().date(),
-        input_formats=['%Y-%m-%d'],
-        widget=forms.DateInput(format='%Y-%m-%d'))
+    fechainicio = forms.CharField(max_length=10, label="Inicio ",
+        widget=(forms.TextInput(attrs={"id": "fechainicio", "value": fecha})))
+
+    fechafin = forms.CharField(max_length=10, label="Fin ",
+         widget=(forms.TextInput(attrs={"id": "fechafin", "value": fecha})))
+
 
     promocion = forms.ChoiceField(choices=promocionChoice())
 
@@ -46,15 +54,11 @@ class FormPublicidadInsert(forms.Form):
 
 class FormPublicidadUpdate(forms.Form):
 
-    fechainicio = forms.DateField(label=("Inicio"),
-      initial=timezone.now().date(),
-      input_formats=['%Y-%m-%d'],
-      widget=forms.DateInput(format='%Y-%m-%d'))
+    fechainicio = forms.CharField(max_length=10, label="Inicio ",
+        widget=(forms.TextInput(attrs={"id": "fechainicio"})))
 
-    fechafin = forms.DateField(label=("Fin"),
-       initial=timezone.now().date(),
-       input_formats=['%Y-%m-%d'],
-       widget=forms.DateInput(format='%Y-%m-%d'))
+    fechafin = forms.CharField(max_length=10, label="Fin ",
+        widget=(forms.TextInput(attrs={"id": "fechafin"})))
 
     promocion = forms.ChoiceField(choices=promocionChoice())
 
