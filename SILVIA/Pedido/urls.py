97601acaf4921d9ed7494225a123b5695eb4752d
iiventura <ireneven@ucm.es>
2019-03-08 09:33:19 +0100

    Configuracion Interna Pedido

--- SILVIA/Pedido/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('nuevoPedido', views.nuevo, name='app_nuevoPedido'),
-    path('modificarPedido', views.modificar, name='app_modificarPedido'),
-    path('borrarPedido', views.borrar, name='app_borrarPedido'),
-    path('listarPedidos', views.listar, name='app_listarPedidos'),
-]
