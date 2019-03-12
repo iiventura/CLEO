ff3586251718cf27e2f59c340db01a83969cd850
iiventura <ireneven@ucm.es>
2019-03-12 21:13:40 +0100

    Preparing Modules

--- apps/urls.py
+++ apps/urls.py
@@ -1,21 +1,17 @@
-"""apps URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #path('cleo/', include('modules.Main.urls')),
+    #path('empleado/',include('modules.Empleado.urls')),
+    #path('sala/',include('modules.Sala.urls')),
+    #path('maquina/',include('modules.Maquina.urls')),
+    #path('tratamiento/',include('modules.Tratamiento.urls')),
+    #path('producto/',include('modules.Producto.urls')),
+    #path('proveedor/',include('modules.Proveedor.urls')),
+    #path('promocion/',include('modules.Promocion.urls')),
+    #path('publicidad/',include('modules.Publicidad.urls')),
+    #path('cliente/',include('modules.Cliente.urls')),
+    #path('notificacion/',include('modules.Notificacion.urls')),
 ]
