5f46a70021869a090ac80e0d4e1bfd10d6609d46
iiventura <ireneven@ucm.es>
2019-03-12 20:30:46 +0100

    Reset System

--- modules/Main/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='app_index'),
-    #path('/about', views.about, name='app_about'),
-
-
-]
--- modules/Sala/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Sala(models.Model):
-    id = models.AutoField(primary_key=True)
-    nombre = models.CharField(max_length=45, blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'Sala'
