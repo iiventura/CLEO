dcf416b39e203fa9c557143a08287c06a0a3e28d
iiventura <ireneven@ucm.es>
2019-03-12 23:26:47 +0100

    Testing Module Empleado

--- apps/urls.py
+++ apps/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     #path('cleo/', include('modules.Main.urls')),
-    #path('empleado/',include('modules.Empleado.urls')),
+    path('empleado/',include('modules.Empleado.urls')),
     #path('sala/',include('modules.Sala.urls')),
     #path('maquina/',include('modules.Maquina.urls')),
     #path('tratamiento/',include('modules.Tratamiento.urls')),
---
+++ modules/Empleado/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.main, name='app_main'),
+    path('nuevo', views.nuevo, name='app_nuevo'),
+    path('lista', views.listar, name='app_listar'),
+    path('<int:pk>', views.perfil, name='app_perfil'),
+    path('<int:pk>/eliminar', views.eliminar, name='app_eliminar'),
+    path('<int:pk>/modificar', views.modificar, name='app_modificar'),
+]
