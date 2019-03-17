225300a993938769b1be830b2a5c479ad7524c81
iiventura <ireneven@ucm.es>
2019-03-17 12:31:15 +0100

    Configuracion Tratamiento

--- apps/urls.py
+++ apps/urls.py
@@ -8,10 +8,15 @@ urlpatterns = [
     path('sala/',include('modules.Sala.urls')),
     path('maquina/',include('modules.Maquina.urls')),
     path('tratamiento/',include('modules.Tratamiento.urls')),
-    #path('producto/',include('modules.Producto.urls')),
+    path('producto/',include('modules.Producto.urls')),
     #path('proveedor/',include('modules.Proveedor.urls')),
     #path('promocion/',include('modules.Promocion.urls')),
     #path('publicidad/',include('modules.Publicidad.urls')),
     #path('cliente/',include('modules.Cliente.urls')),
     #path('notificacion/',include('modules.Notificacion.urls')),
 ]
+"""
+datosMaquina = listaMaquinas("Selecciona")
+      datosProducto = listaProducto("Selecciona")
+
+"""
--- modules/Tratamiento/admin.py
+++ modules/Tratamiento/admin.py
@@ -1,6 +1,3 @@
 from django.contrib import admin
 
-from .models import Tratamiento
-
-admin.site.register(Tratamiento)
-
+# Register your models here.
