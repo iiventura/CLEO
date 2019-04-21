645ddb1714d1e6e9aeff6aac7730b228370267e4
iiventura <ireneven@ucm.es>
2019-04-21 14:23:38 +0200

    Configuracion Usuarios en Admin e Imagen Empleados

--- usuarios/models.py
+++ usuarios/models.py
@@ -10,12 +10,16 @@ class CustomUser(AbstractUser):
 class TipoEmpleado(models.Model):
     nombre = models.CharField(max_length=45, blank=True, null=True)
 
+    def __str__(self):
+        return f'{self.nombre } Tipo '
+
     class Meta:
         db_table = 'tipoempleado'
 
 
 class Empleado(models.Model):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, primary_key=True)
+    foto = models.ImageField(default='default.jpg', upload_to='profile_pics')
     dni = models.CharField(unique=True, max_length=9)
     codigo = models.CharField(max_length=45, blank=True, null=True)
     direccion = models.CharField(max_length=45, blank=True, null=True)
@@ -23,6 +27,8 @@ class Empleado(models.Model):
     tipoempleado = models.ForeignKey('Tipoempleado', models.DO_NOTHING, db_column='tipoEmpleado_id')
 
 
+    def __str__(self):
+        return f'{self.user.first_name  } {self.user.last_name  } Perfil'
 
     class Meta:
         db_table = 'empleado'
@@ -36,17 +42,19 @@ class Cliente(models.Model):
     telefono = models.CharField(max_length=9, blank=True, null=True)
     puntuacion = models.IntegerField(blank=True, null=True)
 
+    def __str__(self):
+        return f'{self.user.first_name  } {self.user.last_name  } Perfil'
 
     class Meta:
        db_table = 'cliente'
 
 
 
-def cargar():
+def cargar_TipoEmpleado():
     if TipoEmpleado.objects.count()==0:
         TipoEmpleado(nombre='Encargado').save()
         TipoEmpleado(nombre='Basico').save()
 
 
 
-cargar()
+cargar_TipoEmpleado()
