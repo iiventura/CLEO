a0dbfda338c1102cd690cec0e3fe4690ca89294d
iiventura <ireneven@ucm.es>
2019-05-09 16:36:44 +0200

    Listar Empleado Authenticado

--- usuarios/empleado/views.py
+++ usuarios/empleado/views.py
@@ -2,11 +2,31 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-#from .models import Empleado, TipoEmpleado
+from usuarios.models import CustomUser,Empleado, TipoEmpleado
 
 @login_required()
 def listar(request):
-    pass
+    datos = Empleado.objects.all()
+    lista = []
+    #lunes = fechaLunes()
+
+
+    for empleado in datos:
+        user = CustomUser.objects.get(empleado=empleado)
+        #turno = turnoEmpleado(lunes,empleado.id)
+
+        data={
+            "id": user.id,
+            "nom": user.first_name,
+            "ape": user.last_name,
+            "email": user.email,
+            "tipo": empleado.tipoempleado.nombre,
+            #"turno": turno,
+        }
+
+        lista.append(data)
+
+    return render(request,'empleado/lista.html',{"lista":lista})
 
 @login_required()
 def eliminar(request):
