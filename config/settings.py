de043d2512ee2b413cc3e57653eb9410b88afbce
iiventura <ireneven@ucm.es>
2019-05-26 12:01:26 +0200

    Configuration

--- config/settings.py
+++ config/settings.py
@@ -31,17 +31,17 @@ INSTALLED_APPS = [
     'modules.apps.horarioEmpleado',
     'modules.apps.maquina',
     'modules.apps.sala',
-    #'modules.apps.promocion',
-    #'modules.apps.publicidad',
-    #'modules.apps.notificacion',
+    'modules.apps.promocion',
+    'modules.apps.publicidad',
+    'modules.apps.notificacion',
     'modules.apps.proveedor',
     'modules.apps.producto',
     'modules.apps.stock',
     'modules.apps.pedido',
     'modules.apps.inventario',
     'modules.apps.tratamiento',
-    #'modules.apps.cita',
-    #'modules.apps.factura',
+    'modules.apps.cita',
+    'modules.apps.factura',
 ]
 
 MIDDLEWARE = [
